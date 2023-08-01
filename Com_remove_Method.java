f9c9fab864238c0ebd7293a061037a5aa336fa5a
Mahi03041997 <solankimandher@gmail.com>
2023-08-01 12:34:50 +0530

    collection interface methods are included (add, addall, remove, removeall, contains, containsall, isempty, clear)

--- Com_remove_Method.java
+++ Com_remove_Method.java
@@ -9,6 +9,7 @@ public class Com_remove_Method {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 List li = new ArrayList<>();
+// add method
 li.add(22);
 li.add(24);
 li.add(74);
@@ -16,12 +17,29 @@ li.add(32);
 List<Integer> list = new ArrayList<>();
 list.add(300);
 list.add(400);
+// add all method
 list.addAll(li);
 System.out.println(list);
+// remove method
 list.remove(3);
 System.out.println(list);
+// remove method
 list.removeAll(li);
 System.out.println(list);
+// contains method
+System.out.println(list.contains(300));
+System.out.println(list.contains(20));
+// contains all method
+System.out.println(list.containsAll(list));
+// is empty method
+System.out.println(list.isEmpty());
+// clear method
+list.clear();
+System.out.println(list);
+System.out.println(list.isEmpty());
+
+
+
 	}
 
 }
